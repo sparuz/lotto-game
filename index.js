cb65558e70ab90c1ae3441e65e5c8509bfba08a4
sparuz <77127628+sparuz@users.noreply.github.com>
2023-12-17 10:09:47 +0100

    feat(m6): update readme file and index comments

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 /*
-
-Ambo = 2 numeri estratti
-Ambetto = estratto uno dei numeri giocati più il precedente o successivo dell'altro numero.
-Terno = 3 numeri estratti
-Quaterna = 4 numeri estratti
-Cinquina = 5 numeri estratti
-
-Possibilita di numeri da giocare da 1 a massimo 10
-
-Ruote = Bari, Cagliari, Firenze, Genova, Milano, Napoli, Palermo, Roma, Torino, Venezia e tutte
-
-Massimo numero di schedine giocabili min: 1, max: 5, 0: exit
+Esempio del display della schedina
 
 +==========================================================+
 |                   LOTTO GAME TICKET #2                   |
